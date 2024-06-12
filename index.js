c4eef2abbc15e7d5a68d7e2ef979eb60830327d2
dbeee <dbeee.acharya@gmail.com>
2024-06-12 16:29:06 +0545

    Create basic layout with add book modal

---
+++ index.js
@@ -0,0 +1,10 @@
+const addBookButton = document.querySelector('.add-book-button-main');
+const addBookFormModal = document.querySelector('.add-book-form-modal');
+const bookListSection = document.querySelector('.book-list-section');
+const addBookSection = document.querySelector('.add-book-section');
+
+addBookButton.addEventListener("click", ()=>{
+    addBookFormModal.classList.toggle("active");
+    bookListSection.classList.toggle("blur");
+    addBookSection.classList.toggle('blur');
+})
