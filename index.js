1db624f1b3a472c5d27fc6c2f57f6d29430dc668
dbeee <dbeee.acharya@gmail.com>
2024-06-12 17:13:34 +0545

    Complete pop up modal design and js

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const addBookButton = document.querySelector('.add-book-button-main');
 const addBookFormModal = document.querySelector('.add-book-form-modal');
 const bookListSection = document.querySelector('.book-list-section');
 const addBookSection = document.querySelector('.add-book-section');
+const closeModalButton = document.querySelector('.close-modal-button');
 
 addBookButton.addEventListener("click", ()=>{
+    addBookFormModal.classList.toggle("active");
+    bookListSection.classList.toggle("blur");
+    addBookSection.classList.toggle('blur');
+})
+
+closeModalButton.addEventListener("click", ()=>{
     addBookFormModal.classList.toggle("active");
     bookListSection.classList.toggle("blur");
     addBookSection.classList.toggle('blur');
